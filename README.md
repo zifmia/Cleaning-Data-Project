af74495a7a8605b1d21bc74d78f7531f20075fb9
zifmia <zifmia7@gmail.com>
2014-06-08 20:11:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting and Cleaning Data Class Project
+
+## Creates a summary data set from the UCI HAR Dataset
---
+++ run_analysis.R
@@ -0,0 +1,5 @@
+trainSubjects = read.table("UCI HAR Dataset/train/subject_train.txt")
+testSubjects  = read.table("UCI HAR Dataset/test/subject_test.txt")
+
+trainData = read.table("UCI HAR Dataset/train/X_train.txt")
+testData  = read.table("UCI HAR Dataset/test/X_test.txt")
