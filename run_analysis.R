cc9f3d284f5190b81c1d4e8d2b4d8aae3de50ea9
zifmia <zifmia7@gmail.com>
2014-06-10 20:25:33 -0500

    analysis complete, documentation missing

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,40 @@
+library(plyr)
+
+# combine training and test subject codes
 trainSubjects = read.table("UCI HAR Dataset/train/subject_train.txt")
 testSubjects  = read.table("UCI HAR Dataset/test/subject_test.txt")
+allSubjects = rbind(trainSubjects, testSubjects)
 
-trainData = read.table("UCI HAR Dataset/train/X_train.txt")
-testData  = read.table("UCI HAR Dataset/test/X_test.txt")
-
+# combine training and test activity codes
 trainActivities = read.table("UCI HAR Dataset/train/y_train.txt")
 testActivities  = read.table("UCI HAR Dataset/test/y_test.txt")
-
-featureNames = read.table("UCI HAR Dataset/features.txt")
-
-keep = grep("(mean|std)\\(\\)", featureNames$V2)
-names = gsub("[()]", "", featureNames[keep,2])
-
-allData = rbind(trainData, testData)
-allData = allData[,keep]
-
-allSubjects = rbind(trainSubjects, testSubjects)
 allActivities = rbind(trainActivities, testActivities)
 
+# combine training and test accelerometer data
+trainData = read.table("UCI HAR Dataset/train/X_train.txt")
+testData  = read.table("UCI HAR Dataset/test/X_test.txt")
+allData = rbind(trainData, testData)
+
+# read in feature names, and select out the mean and standard deviation measurements
+allFeatureNames = read.table("UCI HAR Dataset/features.txt")
+variablesToKeep = grep("(mean|std)\\(\\)", allFeatureNames$V2)
+featureNames = gsub("[()]", "", allFeatureNames[variablesToKeep,2])
+
+# restrict accelerometer data to desired measurements
+allData = allData[,variablesToKeep]
+
+# add subject and activity codes to accelerometer data set
 allData = cbind(allSubjects, allActivities, allData)
-names = c("Subject", "Activity", names)
+colnames(allData) = c("Subject", "Activity", featureNames)
 
-colnames(allData) = names
-
+# reduce data by taking averages of all variables grouped by test subject and activity
 subjectAndActivity = interaction(allSubjects$V1, allActivities$V1)
 splitData = split(allData, subjectAndActivity)
+summaryData = data.frame(laply(splitData, colMeans))
 
-reducedData = lapply(splitData, colMeans)
+# replace activity codes with descriptive labels
+activityLabels = read.table("UCI HAR Dataset/activity_labels.txt")
+summaryData$Activity = activityLabels[match(summaryData$Activity, activityLabels$V1),2]
 
-print("Still need to replace activity numbers with names")
+# write out summary data set
+write.table(summaryData, "summaryAccelerometerData.txt", row.names=FALSE, quote=FALSE)
